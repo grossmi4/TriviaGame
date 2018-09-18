004f4f889d8ecb25daa43e73b71e7b569ec5d93e
Mike Gross <grossmi4@gmail.com>
2018-09-18 16:01:10 -0400

    minor changes to css, still ugly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,24 +2,31 @@ let intervalId;
 
 const quiz = {
   questions: [
-    {question: `Placeholder question 1`,
-     answers: [`placeholder answer 1`, `placeholder answer 2`, `placeholder answer 3`],
-      correctAnswer: `placeholder answer 1`
+    {question: `Who is known as the 'father of botany'?`,
+     answers: [`Aristotle`, `Mendel`, `Theophrastus`],
+      correctAnswer: `Theophrastus`
     },
-    {question: `Placeholder question 2`,
-      answers: [`placeholder answer 1`, `placeholder answer 2`, `placeholder answer 3`],
-      correctAnswer: `placeholder answer 2`
+    {question: `In geology, what is the name given to naturally occuring molten rock material generated and found within the Earth?`,
+      answers: [`Lava`, `Magma`, `Igneous Rock`],
+      correctAnswer: `Magma`
     },
-    {question: `Placeholder question 3`,
-      answers: [`placeholder answer 1`, `placeholder answer 2`, `placeholder answer 3`],
-      correctAnswer: `placeholder answer 3`
+    {question: `All plant leaves are connected to the stem by a thin stalk. What is this stalk called?`,
+      answers: [`Stem`, `Blade`, `Petiole`],
+      correctAnswer: `Petiole`
+    },
+    {question: `Which unit of energy was named in honour of an English scientist of the 19th century?`,
+      answers: [`Hertz`, `Joule`, `Farad`],
+      correctAnswer: `Petiole`
+    },
+    {question: `What substance produced from the plant is used to create its cell walls?`,
+      answers: [`Lignin`, `Ligature`, `Cellulose`],
+      correctAnswer: `Cellulose`
     }
   ]
 };
 
 let game = {
   clockTimer: 0,
-  intervalId,
   questionIndex: 0,
   correctCount: 0,
   incorrectCount: 0,
@@ -66,16 +73,17 @@ let game = {
    $("#answers").empty();
    $("#question").text(quiz.questions[qIndex].question);
    for(answer of quiz.questions[qIndex].answers) {
-     let newAnswer = $("<span class='answer'>");
-     newAnswer.text(answer);
-     $("#answers").append(newAnswer).append("<br>");
+     let newAnswer = $("<span class='answer'>").text(answer);
+     let newDiv = $("<div class='answerContainer'>");
+     newDiv.append(newAnswer);
+     $("#answers").append(newDiv);
    }
    $(document).on("click",".answer",this.selectAnswer); //add listener
    this.startQTimer()
   },
 
   startQTimer: function() {
-    game.clockTimer = 30;
+    game.clockTimer = 10;
     $("#timer").text(game.clockTimer);
     intervalId = setInterval(game.clockStep,1000);
   },
@@ -90,6 +98,9 @@ let game = {
   },
 
   timeOut: function() {
+    $(document).off("click",".answer",this.selectAnswer);
+    game.questionIndex++;
+    game.incorrectCount++;
     $("#after-question").text("Out of time!");
     setTimeout(function(){
       $("#after-question").empty();
